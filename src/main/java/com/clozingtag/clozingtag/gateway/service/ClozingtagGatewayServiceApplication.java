33ec3336d87ba2d58e1de67fc88ab6a2dbeaea54
Saint Deemene <deemene.saint@loblaw.ca>
2025-03-04 11:50:12 +0000

    Project Setup

---
+++ src/main/java/com/clozingtag/clozingtag/gateway/service/ClozingtagGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clozingtag.clozingtag.gateway.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClozingtagGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClozingtagGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clozingtag/clozingtag/gateway/service/ClozingtagGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clozingtag.clozingtag.gateway.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClozingtagGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
